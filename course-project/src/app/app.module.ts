bfc5600505b0e63f252f2ddfbc5e1f62798d7e94
Niall <nod652@gmail.com>
2017-06-13 13:13:27 +0100

    Followed through to end of Course Project section 3

--- course-project/src/app/app.module.ts
+++ course-project/src/app/app.module.ts
@@ -1,13 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
+﻿import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeItemComponent } from './recipes/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    RecipesComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    RecipeListComponent
   ],
   imports: [
     BrowserModule,
---
+++ course-project/src/app/recipes/recipe.model.ts
@@ -0,0 +1,11 @@
+﻿export class Recipe {
+  public name: string;  
+  public description: string;
+  public imagePath: string;
+
+  constructor(name: string, description: string, imagePath: string) {
+    this.name = name;
+    this.description = description;
+    this.imagePath = imagePath;
+  }
+}
